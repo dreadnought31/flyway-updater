d860473b42f66080a8748288bfbbe7ccbc35adb1
Dreadnought31 <87851506+dreadnought31@users.noreply.github.com>
2023-09-13 08:51:45 +0100

    Update flyway-serverupdater.ps1

--- flyway-serverupdater.ps1
+++ flyway-serverupdater.ps1
@@ -4,6 +4,7 @@
 # Ver 1.0 - AOB Created 11-09-2023
 # Ver 1.1 - AOB 12-09-2023 Added new script section: add the installation folder to "Path" in System Environment Variables if not already there.
 # Ver 1.2 - AOB 12-09-2023 Added Enforcement of TLS 1.2 and updated download folder path.
+# Ver 1.3 - AOB 12-09-2023 Updated Parameters section with paths for the script
 
 Start-Transcript -Path "D:\flywayinstall.txt" -append
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
@@ -11,13 +12,13 @@ Start-Transcript -Path "D:\flywayinstall.txt" -append
 $onlineVersion = "9.22.1"
 $URL = “https://repo1.maven.org/maven2/org/flywaydb/flyway-commandline/$onlineVersion/flyway-commandline-$onlineVersion.zip”
 $Path= ”D:\temp\downloads\flyway-commandline-$onlineVersion.zip”
-$destination = "D:\temp\Flyway"
 $flywayInstallPath = "D:\Flyway"
+$Extractionpath = "D:\temp"
 $FolderName = "D:\temp\Flyway\"
 $Downloads =  "D:\temp\downloads"
 
 # Add the location of the flyway folder to System Environment Variable if it is needed, if already there it will skip this.
-$newPath = "D:\flyway"
+$newPath = "$flywayInstallPath"
 
 # Get the current value of the Path variable
 $currentPath = [System.Environment]::GetEnvironmentVariable("Path", [System.EnvironmentVariableTarget]::Machine)
@@ -37,7 +38,7 @@ if ($currentPath -notlike "*$newPath*") {
 
 # Clean up the old Flyway installation
 Write-Host "Cleaning up old Flyway installation" -ForegroundColor Green
-Remove-Item –path D:\flyway\* –recurse
+Remove-Item –path $flywayInstallPath\* –recurse
 
 # Check if folder exist and create if needed, then Download the latest Flyway version
 if (Test-Path $FolderName) {
@@ -70,17 +71,17 @@ else
 }
 Invoke-WebRequest -URI $URL -OutFile "$Downloads\flyway-commandline-$onlineVersion.zip"
 Write-Host "Unzip the file and output to temp folder" -ForegroundColor Green
-Expand-Archive -Path "$Path" -DestinationPath "D:\temp\" -Force
+Expand-Archive -Path "$Path" -DestinationPath "$Extractionpath\" -Force
 
 # Copy all file in D:\temp to flyway folder
 Write-Host "Copy all file in D:\temp to flyway folder" -ForegroundColor Green
-Copy-Item -Path "D:\temp\flyway*\*" -Destination D:\flyway -Recurse
+Copy-Item -Path "$Extractionpath\flyway*\*" -Destination $flywayInstallPath -Recurse
 
 # Cleanup download and temp files
 Write-Host "Cleanup download and temp files" -ForegroundColor Green
-Remove-Item –path "D:\temp\*" –recurse
+Remove-Item –path "$Extractionpath\*" –recurse
 
 # Confirm new version is installed
 Write-Host "Confirm Flyway is updated to newest version" -ForegroundColor Green
-cmd.exe /c D:\flyway\flyway --version
+cmd.exe /c $flywayInstallPath\flyway --version
 Stop-Transcript
