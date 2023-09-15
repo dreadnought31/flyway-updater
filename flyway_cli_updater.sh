eeb1b7d5931219c103367371bed866dab580d310
Dreadnought31 <87851506+dreadnought31@users.noreply.github.com>
2023-09-15 14:20:53 +0100

    Create flyway_cli_updater.sh

---
+++ flyway_cli_updater.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#FlywayCommunity Updater Ver 1 by Alan O'Brien
+# Define the Flyway download URL
+FLYWAY_DOWNLOAD_URL="https://repo1.maven.org/maven2/org/flywaydb/flyway-commandline"
+current_version="9.22.1"
+online_version="9.22.1"
+
+#Delete current Flyway directory
+sudo rm -r "/opt/flyway/$current_version"
+echo "Delete current Flyway directory"
+
+# Define the installation directory
+install_dir="/opt/flyway/$online_version"
+echo "Define the installation directory"
+
+
+# Create the installation directory if it doesn't exist
+sudo mkdir -p "$install_dir"
+echo "Create the installation directory if it doesn't exist"
+
+# Download and extract the Flyway archive
+download_link="$FLYWAY_DOWNLOAD_URL/$online_version/flyway-commandline-$online_version-linux-x64.tar.gz"
+wget -qO- "$download_link" | sudo tar -xz -C "$install_dir" --strip-components=1
+echo "Flyway $online_version has been downloaded and installed to $install_dir."
+
+#Delete Old Symbolic Link if there
+echo "Delete Old Symbolic Link if there"
+sudo unlink /usr/local/bin/flyway
+
+# Create symbolic link for easy access
+sudo ln -s "$install_dir/flyway" /usr/local/bin/flyway
+echo "Symbolic link to flyway created in /usr/local/bin."
+
+# Confirm new installed version
+echo "Confirm new installed version"
+flyway --version
